9750424edfcdceb16a45b8998794db8da5540594
AlexandrRust <123_00@ukr.net>
2022-10-22 15:47:46 +0300

    fix-jimp-22-10

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -8,13 +8,12 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
-  const { file } = req;
-  const avatar = await jimp.read(file.path);
+  const { path: tempUpload, originalname } = req.file;
+  const avatar = await jimp.read(tempUpload);
   avatar
     .autocrop()
     .cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE)
-    .writeAsync(file.path);
-  const { path: tempUpload, originalname } = req.file;
+    .writeAsync(tempUpload);
   const extention = originalname.split(".").pop();
   const filename = `${_id}.${extention}`;
   const resultUpload = path.join(avatarsDir, filename);
